8308f26b5b5e553d38a7513d79f7e58cbd400541
Xusrav <xusrav200329@gmail.com>
2023-06-06 16:58:57 +0500

    Создал обработчик хенлера setlang который возвращает callback data, и создал еще один хендлер для принятия текстов

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
 from KEY import API
-from aiogram.types import (InlineKeyboardMarkup,
-                           InlineKeyboardButton,
-                           ReplyKeyboardMarkup,
-                           KeyboardButton)
+from deep_translator import GoogleTranslator
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = Bot(API)
 dp = Dispatcher(bot)
+
+lang_from = "ru"
+lang_to = "en"
 
 
 @dp.message_handler(commands=['start'])
@@ -20,7 +21,7 @@ async def set_lang(message: types.Message):
     keyboard = InlineKeyboardMarkup(row_width=3)
     kb1 = InlineKeyboardButton("Русский/Английский", callback_data="ru/en")
     kb2 = InlineKeyboardButton("Русский/Узбекский", callback_data="ru/uz")
-    kb3 = InlineKeyboardButton("Русский/Китайский", callback_data="ru/cn")
+    kb3 = InlineKeyboardButton("Русский/Китайский", callback_data="ru/zh-CN")
     kb4 = InlineKeyboardButton("Английский/Узбекский", callback_data="en/uz")
     kb5 = InlineKeyboardButton("Узбекский/Английский", callback_data="uz/en")
     kb6 = InlineKeyboardButton("Английский/Русский", callback_data="en/ru")
@@ -29,5 +30,48 @@ async def set_lang(message: types.Message):
     await message.answer("Выберите язык", reply_markup=keyboard)
 
 
+@dp.callback_query_handler()
+async def translator(callback: types.CallbackQuery):
+    global lang_to
+    global lang_from
+    if callback.data == "ru/en":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+    elif callback.data == "ru/uz":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+    elif callback.data == "ru/zh-CN":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+    elif callback.data == "en/uz":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+    elif callback.data == "uz/en":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+    elif callback.data == "en/ru":
+        lang_to = callback.data[:2]
+        lang_from = callback.data[3:]
+        await callback.answer("Вы успешно сменили язык, теперь введите свой текст!")
+
+
+@dp.message_handler(content_types=['text'])
+async def only_text(message: types.Message):
+    text = message.text
+    translated = GoogleTranslator(source=lang_to, target=lang_from)
+    translated_text = translated.translate(text)
+    await message.reply(
+        text=f"<b>Перевод:</b> <i>{translated_text}</i> \n "
+             f"\n"
+             f"<b>Оригинал:</b> <u>{text}</u>",
+        parse_mode="HTML"
+    )
+
+
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
